e5833eb9a0c23420cda2ac043689019204fd277e
Valentine124 <kaluvalentine60@gmail.com>
2024-09-03 06:20:31 +0000

    Added some file to start with

---
+++ models/google_api.py
@@ -0,0 +1,4 @@
+"""
+This module contains functions that retrieves data
+from the google books and search APIs
+"""
---
+++ models/udemy_api.py
@@ -0,0 +1,4 @@
+"""
+The module contain function that retrieves data from
+the Udemy API
+"""
